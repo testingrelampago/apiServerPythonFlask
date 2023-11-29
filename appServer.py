aa13c8fc7ab634a8035106dc3f8019a348a32e22
Testing Relampago <151034626+testingrelampago@users.noreply.github.com>
2023-11-29 15:36:02 +0100

    Server

--- appServer.py
+++ appServer.py
@@ -1,14 +1,24 @@
 # ----- Before the import, you should install Flask
 # ----- pip install Flask
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 # ----- Define a simple route
 @app.route('/')
-def homeSite():
-    return 'Hello, World! This is your API server.'
+def home():
+    return render_template("home.html")
+
+# ----- Route with out JSON response
+@app.route("/buenosAires")
+def buenosAires():
+    return render_template("buenosAires.html")
+    
+# ----- Route with out JSON response
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 # ----- Define a route with JSON response
 @app.route('/api/data', methods=['GET'])
